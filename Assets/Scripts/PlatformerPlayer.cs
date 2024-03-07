f0412500866e6d3e976e231d6bc71f5dcc328b6a
Boganabi <logan.ashbaugh0001@gmail.com>
2024-03-07 14:15:54 -0800

    finished part 2 platformer

--- Assets/Scripts/PlatformerPlayer.cs
+++ Assets/Scripts/PlatformerPlayer.cs
@@ -6,15 +6,18 @@ public class PlatformerPlayer : MonoBehaviour
 {
 
     public float speed = 4.5f;
+    private float jumpForce = 12.0f;
 
     private Rigidbody2D body;
     private Animator anim;
+    private BoxCollider2D box;
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        box = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -24,10 +27,28 @@ public class PlatformerPlayer : MonoBehaviour
         Vector2 movement = new Vector2(deltaX, body.velocity.y);
         body.velocity = movement;
 
+        Vector3 max = box.bounds.max;
+        Vector3 min = box.bounds.min;
+        Vector2 corner1 = new Vector2(max.x, min.y - 0.1f);
+        Vector2 corner2 = new Vector2(min.x, min.y - 0.2f);
+        Collider2D hit = Physics2D.OverlapArea(corner1, corner2);
+
+        bool grounded = hit != null; // false
+        //if(hit != null)
+        //{
+        //    grounded = true;
+        //}
+
         anim.SetFloat("Speed", Mathf.Abs(deltaX));
         if(!Mathf.Approximately(deltaX, 0))
         {
             transform.localScale = new Vector3(Mathf.Sign(deltaX), 1, 1);
+        }
+
+        body.gravityScale = (grounded && Mathf.Approximately(deltaX, 0)) ? 0 : 1;
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
 }
